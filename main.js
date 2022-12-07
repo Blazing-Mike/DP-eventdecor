5b49b18ede80505642b192999bd8b0b1f3d1c3ae
Michael Adebambo <adebambomichael5@gmail.com>
2022-12-07 21:09:53 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('.nav');
+const header = document.querySelector('.header');
+
+
+menuBtn.addEventListener('click', () => {
+header.classList.toggle("open")
+//nav.style.left = "0%";
+  
+});
